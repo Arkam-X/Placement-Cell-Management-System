e21e70641a57e0c05e77031e527ac3bab5cc4246
Arkam-X <charkam786@gmail.com>
2025-12-15 01:30:40 +0530

    Structured the Directory, Server is running successfully.

---
+++ backend/src/server.js
@@ -0,0 +1,12 @@
+const express = require("express");
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send("Placement Cell Backend is running! BoomBaam");
+});
+
+const PORT = 5000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
